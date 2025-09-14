12aae191cddf4b0f3e82596b2be65ae10b08402a
MariamMahmoudMohamedIbrahem <mariam.mahmoud2811@gmail.com>
2025-09-14 15:29:27 +0300

    TEST: Simple script to verify Xcode Cloud detection

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-set -e
-
-echo "=== Running ci_post_clone.sh ==="
-echo "Script path: $(pwd)/$0"
-
-# Install Flutter dependencies
-echo "Running flutter pub get..."
-flutter pub get
-
-# Install iOS pods
-echo "Running pod install..."
-cd ios
-pod install --repo-update
-cd ..
-
-echo "=== CI setup completed successfully ==="
+echo "=== SCRIPT IS RUNNING! ==="
+echo "If you see this, the script was found!"
+exit 0
